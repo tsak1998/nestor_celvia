4070c1733a4e3138df1b434db7dd1312301fdf64
ichamatidis <ichamatidis@gmail.com>
2024-11-30 19:13:50 +0200

    VAE individual

--- src/vae_individual.py
+++ src/vae_individual.py
@@ -6,11 +6,11 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-path_feature = os.path.abspath('F:/extracted_embeddings_pretrained_concat')
+path_feature = os.path.abspath('E:/extracted_embeddings_pretrained_concat')
 save_path = os.path.abspath('F:/latent_vectors_individual/')
 model_save_path = os.path.abspath('C:/Users/user/Documents/nestor_celvia/best_vae_all/')
 device = 'cuda:0'
-
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
 class VAE(nn.Module):
     def __init__(self, latent_dim=1024):
@@ -92,37 +92,51 @@ class CustomDataset(Dataset):
         data = torch.load(os.path.join(self.data_dir+f"/{self.data_names[idx]}"))
         return {'data':data, 'id':self.data_names[idx].split('.')[0]}
 
-# Train
-train_dataset = CustomDataset(path_feature)
-trainloader = DataLoader(train_dataset, batch_size=1, shuffle=False)
-vae = VAE(latent_dim=1024).to(device)
-optimizer = torch.optim.Adam(vae.parameters(), lr=1e-3)
-min_loss = float('inf')
-num_epochs = 100
-for epoch in tqdm(range(num_epochs)):
-    for data in trainloader:
-        # Forward pass
-        data = data['data'].squeeze(0).to(device)
-        reconstructed, mu, logvar, _ = vae(data)  # concat_t shape: [88, 256, 64, 64]
-        # Loss computation
-        loss = vae_loss(reconstructed, data, mu, logvar)
-        loss_np = loss.detach().cpu().numpy().item()
+
+def train_model():
+    # Train
+    train_dataset = CustomDataset(path_feature)
+    trainloader = DataLoader(train_dataset, batch_size=1, shuffle=False, pin_memory=True)
+    vae = VAE(latent_dim=1024).to(device)
+    optimizer = torch.optim.Adam(vae.parameters(), lr=1e-3)
+    min_loss = float('inf')
+    num_epochs = 20
+    for epoch in tqdm(range(num_epochs)):
+        epoch_loss = 0
+        for data in trainloader:
+            # Forward pass
+            if data['data'].squeeze(0).shape[0] > 1000:
+                continue
+            data = data['data'].squeeze(0).to(device)
+            reconstructed, mu, logvar, _ = vae(data)  # concat_t shape: [88, 256, 64, 64]
+            # Loss computation
+            loss = vae_loss(reconstructed, data, mu, logvar)
+            loss_np = loss.detach().cpu().numpy().item()
+            epoch_loss += loss_np
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
         # Keep best model
-        if loss_np < min_loss:
-            min_loss = loss_np
+        epoch_loss /= len(trainloader)
+        tqdm.write(f"Epoch: {epoch}, loss: {epoch_loss}")
+        if epoch_loss < min_loss:
+            min_loss = epoch_loss
             torch.save(vae.state_dict(),os.path.join(model_save_path+f'/best_vae.pth'))
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-print('Training finished.')
+    print('Training finished.')
 
 # Infer
-vae = VAE(latent_dim=1024).to(device)
-vae.load_state_dict(torch.load(os.path.join(model_save_path+f'/best_vae.pth')))
-vae.eval()
-with torch.no_grad():
-    for data_t in tqdm(trainloader):
-        data = data_t['data'].squeeze(0).to(device)
-        reconstructed, mu, logvar, agrregated = vae(data)
-        torch.save(agrregated, os.path.join(save_path, f"{data_t['id'][0]}.pt"))
-print('VAE finished.')
+def infer_model():
+    train_dataset = CustomDataset(path_feature)
+    trainloader = DataLoader(train_dataset, batch_size=1, shuffle=False)
+    vae = VAE(latent_dim=1024).to(device)
+    vae.load_state_dict(torch.load(os.path.join(model_save_path+f'/best_vae.pth')))
+    vae.eval()
+    with torch.no_grad():
+        for data_t in tqdm(trainloader):
+            data = data_t['data'].squeeze(0).to(device)
+            reconstructed, mu, logvar, agrregated = vae(data)
+            torch.save(agrregated, os.path.join(save_path, f"{data_t['id'][0]}.pt"))
+    print('VAE finished.')
+
+train_model()
+# infer_model()
